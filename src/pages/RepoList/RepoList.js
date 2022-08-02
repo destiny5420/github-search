f60ec9d13953a190da9bb809b92fc41e1d24d92c
paperhsiao(M1 Pro) <destiny5420@gmail.com>
2022-08-02 13:26:08 +0800

    feat: 新增 intersection 的 feature

--- src/pages/RepoList/RepoList.js
+++ src/pages/RepoList/RepoList.js
@@ -1,29 +1,62 @@
 import React, { useEffect, useRef, useState, memo } from 'react'
 import { useParams } from 'react-router-dom'
-import { Box, Typography, Divider } from '@mui/material'
+import { useSelector } from 'react-redux'
 import Repo from 'components/Repo/Repo'
 import { GetRepoList10 } from 'js/api.js'
+import { Box, Typography, Divider } from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress'
 
 const RepoList = () => {
   // State
+
   const [repoData, setRepoData] = useState([])
   const [page, setPage] = useState(1)
 
   // Ref
   const progressRef = useRef(null)
+  const fetchDataDone = useRef(true)
+
+  // Redux
+  const { publicRepoCount } = useSelector((state) => state.user)
 
   const { username } = useParams()
 
   // Effect
   useEffect(() => {
-    const work = async () => {
-      const newRepoList = await GetRepoList10(username, page)
+    const fetchRepoDataBy10 = async () => {
+      fetchDataDone.current = false
+
+      const newRepoList = await GetRepoList10(
+        username,
+        page,
+        process.env.REACT_APP_GITHUB_READ_PROJECT_TOKEN
+      )
 
       setRepoData((old) => [...old, ...newRepoList])
+      fetchDataDone.current = true
     }
 
-    work()
+    fetchRepoDataBy10()
   }, [page])
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && page < publicRepoCount && fetchDataDone.current) {
+          setPage(page + 1)
+        }
+      },
+      { root: null, rootMargin: '0px', threshold: 0 }
+    )
+
+    if (progressRef.current) {
+      observer.observe(progressRef.current)
+    }
+
+    return () => {
+      observer.disconnect()
+    }
+  })
 
   // Dom Element
   const repoElements = repoData.map((data, index) => {
@@ -45,21 +78,24 @@ const RepoList = () => {
 
   console.log(`[REPO-LIST] re-render`)
 
-  return (
+  return repoData.length > 0 ? (
     <>
-      {repoData.length === 0 ? (
-        <Typography
-          textAlign={`center`}
-          variant="h5"
-          color={`#666666`}
-          fontWeight={200}
-          paddingY={`1rem`}>
-          {`Haven't created any repository yet.`}
-        </Typography>
-      ) : (
-        repoElements
+      {repoElements}
+      {10 * page < publicRepoCount && (
+        <Box display={`flex`} justifyContent={`center`} marginBottom={`1rem`}>
+          <CircularProgress ref={progressRef} />
+        </Box>
       )}
     </>
+  ) : (
+    <Typography
+      textAlign={`center`}
+      variant="h5"
+      color={`#666666`}
+      fontWeight={200}
+      paddingY={`1rem`}>
+      {`Haven't created any repository yet.`}
+    </Typography>
   )
 }
 
